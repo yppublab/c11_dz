d10286284a32a225833da90c04ae426a78808068
vigboo <vigboo@gmai.com>
2025-11-09 23:45:17 +0100

    Добавлен лог sshd на pc_bastion

--- pc_bastion/start.sh
+++ pc_bastion/start.sh
@@ -8,6 +8,6 @@ useradd -m -s /bin/bash -p "${PETROVICH_HASH}" petrovich || true
 chmod +x /usr/local/bin/ansible_agent_deploy.sh
 /usr/local/bin/ansible_agent_deploy.sh
 sudo sed -i 's/^#\?Port .*/Port 1832/' /etc/ssh/sshd_config
-/usr/sbin/sshd
+/usr/sbin/sshd -E /var/log/sshd.log
 
 exec tail -f /dev/null
